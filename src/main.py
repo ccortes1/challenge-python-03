d5376d9c50e91b0d70de83be358a0c4452b14925
Cortes <cristianalbertocortesgutierrez@gmail.com>
2020-05-24 08:25:00 -0500

    solucion al reto 3 de python

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 # Resolve the problem!!
-
+import re
 
 def run():
     # Start coding here
-
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        cont2 = ''.join(re.findall(r'[a-z]', f.read()))
+        print(cont2)
 
 if __name__ == '__main__':
     run()
